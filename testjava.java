4f0b8faf70ed1ae03784f5ced115f7cfb5ae92dd
Tsubiha <23203033@srcas.ac.in>
2024-07-01 11:58:06 +0530

    Create testjava.java

---
+++ testjava.java
@@ -0,0 +1,9 @@
+class testjava{
+  public static void main(String[]args)
+  {
+    System.out.println("welcome");
+    System.out.println("monsoon");
+     System.out.println("elephant");
+    
+  }
+}
